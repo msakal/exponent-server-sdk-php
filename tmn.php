606b3713ce7b4a4e5f7f2110f20a4432be9c77eb
Guilherme Santana <g.santossantana22@gmail.com>
2023-01-27 10:19:57 -0300

    finalização desafio

--- tmn.php
+++ tmn.php
@@ -1,8 +1,12 @@
 <?php
  require_once __DIR__.'/vendor/autoload.php';
-    
+
+ if(isset($_POST["enviar"])){
+$titulo = $_POST["titulo"];
+$mensagem = $_POST["mensagem"];
+
  $channelName = 'news';
- $recipient= ['ExponentPushToken[Aak41dPb_OImfGxGCelRfz]','ExponentPushToken[XS_qc1AcdpKNmV_fukD-kY]','ExponentPushToken[wcFi9XPagiMl8Uu_a1KF__]' ];
+ $recipient= ['ExponentPushToken[Aak41dPb_OImfGxGCelRfz]','ExponentPushToken[XS_qc1AcdpKNmV_fukD-kY]' ];
  
  // You can quickly bootup an expo instance
  $expo = \ExponentPhpSDK\Expo::normalSetup();
@@ -13,8 +17,31 @@
      $expo->subscribe($channelName, $valor);
     }
     // Build the notification data
-    $notification = ['title' => 'teste','body' => 'Hello World!'];
+    $notification = ['title' => $titulo,'body' => $mensagem];
     
     // Notify an interest with a notification
     $expo->notify([$channelName], $notification);
+}
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Teste</h1>
+        <form method="post">
+            <label for="">Titulo</label>
+            <input type="text" name="titulo">
+            <label for="">mensagem</label>
+            <input type="text" name="mensagem">
+            <Button name="enviar" > enviar</Button>
+
+        </form>
+    </body>
+    </html>
  
